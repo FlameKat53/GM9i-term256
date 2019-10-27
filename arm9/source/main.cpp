838be685bea2c1cd575f288d89248636ee463887
FlameKat53 <flame9804@yahoo.com>
2019-10-27 09:21:41 -0500

    add boot screen (read desc)
    it will show the boot screen for eternity at the moment, please be paitient while i add the drive menu

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -4,6 +4,7 @@ extern "C" {
 #include "driveOperations.h"
 }
 
+#include "gm9i_logo.h"
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
@@ -21,6 +22,8 @@ term_t t1;
 #define CyanOnBlk "\x1b[32;1;40m"
 #define Red "\x1b[31;1m"
 #define BlkOnRed "\x1b[31;1;7;30m"
+
+static int bg3;
 
 void set_scroll_callback(int x, int y, void *param) {
 	bgSetScroll(*(int*)param, x, y);
@@ -49,10 +52,29 @@ int main(int argc, const char * const argv[]) {
 	term_init(&t0, bg0, set_scroll_callback, &bg0id);
 	term_init(&t1, bg1, set_scroll_callback, &bg1id);
 
-	select_term(&t1);
-
 	cpuStartTiming(0);
+	// Display GM9i logo
+	bg3 = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 1, 0);
+	bgSetScroll(bg3, 0, 0);
+	decompress(gm9i_logoBitmap, bgGetGfxPtr(bg3), LZ77Vram);
 
-		prt("GodMode9i v2.0.0");
+	select_term(&t0);
+	prt("\x1b[5;1H");
+	prt("\tGodMode9i v2.0.0");
+	prt("\x1b[6;1H");
+	prt("\t------------------------------");
+	prt("\x1b[7;1H");
+	prt("\t\thttps:/github.com/\n");
+	prt("\t\t\t\tRocketRobz/GodMode9i");
+	if (isDSiMode()) {
+		prt("\x1b[22;1H");
+		prt("\taaHold Y - Disable cart access");
+	}
 		stop();
+	// Display for 2 seconds
+	for (int i = 0; i < 60*2; i++) {
+		swiWaitForVBlank();
+	}
+
+	fifoWaitValue32(FIFO_USER_06);
 }
