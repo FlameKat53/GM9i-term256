b7b191cd954dcd1b76239ee194351ab005db11ca
FlameKat53 <flame9804@yahoo.com>
2019-10-28 13:02:39 -0500

    Update main.h

--- arm9/include/main.h
+++ arm9/include/main.h
@@ -7,16 +7,22 @@
 #define HOMETEXT 		"HOME - HOME Menu prompt"
 #define SCREENSHOTTEXT 	"R+L - Make a screenshot"
 
+// For Console
+#define Cls "\x1b[2J"
+#define Rst "\x1b[0m"
+#define BlkOnWht "\x1b[30;47m"
+#define CyanOnBlk "\x1b[32;1;40m"
+#define Red "\x1b[31;1m"
+#define BlkOnRed "\x1b[31;1;7;30m"
+
 extern char titleName[32];
 
 extern int screenMode;
 
 extern bool appInited;
-
 extern bool arm7SCFGLocked;
 extern bool isRegularDS;
 extern bool is3DS;
-
 extern bool applaunch;
 
 
